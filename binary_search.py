b488076154f0d1867df3553b6bb6bac64a22fa7d
nico <nico.walters@techtalents.es>
2020-03-07 15:58:10 +0100

    Added assert to binary search

--- binary_search.py
+++ binary_search.py
@@ -1,6 +1,6 @@
 from typing import Any, Optional, Callable, Union, Text
 
-def binary_search(data: list, needle: Any, key: Optional[Callable[Any, Any]] = None) -> Any:
+def binary_search(data: list, needle: Any, key: Optional[Callable[[Any], Any]] = None) -> Any:
     left_index = 0
     right_index = len(data)
 
@@ -8,13 +8,15 @@ def binary_search(data: list, needle: Any, key: Optional[Callable[Any, Any]] = N
         middle_index = (right_index - left_index) // 2 + left_index
         if key:
             datum = key(data[middle_index])
-            
+        else:
+            datum = data[middle_index]
+
         if datum > needle:
             right_index = middle_index
         elif datum < needle:
             left_index = middle_index
         else:
-            return middle_index
+            return middle_index + 1
 
 
 def main(needle):
@@ -24,7 +26,11 @@ def main(needle):
 
 
 
+assert binary_search([4, 8, 15, 16, 23, 42], 15) == 2, "OMG THE WORLD IS ON FIRE"
+
+
+
 if __name__ == "__main__":
-    print(main(6666666))
-    
-    binary_search()
+    # print(main(6666666))
+    pass
+    # binary_search([1,2,3,4,5,6,7,8,9], 5)
